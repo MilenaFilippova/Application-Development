125e687b4106ae0cbd46b9380825f5d718368979
MilenaFilippova <33898241+MilenaFilippova@users.noreply.github.com>
2019-12-20 16:37:17 +0800

    Create FieldsCards.java

---
+++ GameSet/FieldsCards.java
@@ -0,0 +1,18 @@
+package com.example.gameset;
+
+public class FieldCards
+{
+    int count;
+    int color;
+    int shape;
+    int fill;
+
+    public FieldCards()
+    {
+        this.count=count;
+        this.color=color;
+        this.shape=shape;
+        this.fill=fill;
+
+    }
+}
