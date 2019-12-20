a406e65638344484c08fc1f9d100b8303269bbc4
MilenaFilippova <33898241+MilenaFilippova@users.noreply.github.com>
2019-12-20 16:36:43 +0800

    Create GetCards.java

---
+++ GameSet/GetCards.java
@@ -0,0 +1,13 @@
+package com.example.gameset;
+
+import java.util.ArrayList;
+
+public class GetCards
+{
+    //получаем карточки
+    public String status;
+    public ArrayList<FieldCards> datacards = new ArrayList();
+
+    public GetCards() {
+    }
+}
