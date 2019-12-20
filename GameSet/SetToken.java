e5d6fcf8389f4615083f3be22f7ceca501d44a23
MilenaFilippova <33898241+MilenaFilippova@users.noreply.github.com>
2019-12-20 16:38:04 +0800

    Create SetToken.java

---
+++ GameSet/SetToken.java
@@ -0,0 +1,13 @@
+package com.example.gameset;
+
+import java.util.Arrays;
+
+public class SetToken {
+
+    public String status;
+    public Integer tokenNumber;
+
+    public SetToken() {
+    }
+
+}
