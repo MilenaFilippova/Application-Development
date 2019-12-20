ac04f3704bb0cfa6b2582bbc90caeed67b8980b8
MilenaFilippova <33898241+MilenaFilippova@users.noreply.github.com>
2019-12-20 16:37:40 +0800

    Create SetCards.java

---
+++ GameSet/SetCards.java
@@ -0,0 +1,13 @@
+package com.example.gameset;
+
+
+public class SetCards {
+    //отправляем запрос на карточки
+    public String action;
+    public Integer token;
+
+    public SetCards() {
+        this.action = action;
+        this.token = token;
+    }
+}
